9ac905faaf929847303ab8c818a5f6e70c7c97a2
Young <canbbu222@gmail.com>
2024-08-24 18:42:01 +0900

    repository init

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require("express")
+const app = express()
+
+//라우팅
+const home = require("./routes/home")
+const PORT = 3000
+
+//앱 세팅
+app.set("views","./views")
+app.set("view engine","ejs")
+
+//미들웨어
+app.use("/", home)
+
+module.exports = app;
