9d251271b74dc013b4c9272fe18ab3209253a644
canbbu222 <canbbu222@gmail.com>
2024-09-01 13:51:42 +0900

    dotenv

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@
 
 const express = require("express")
 const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+dotenv.config()
+const morgan = require("morgan")
 const app = express()
 
 //라우팅
@@ -15,7 +18,7 @@ app.use(express.static(`${__dirname}/src/public`))
 app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글, 공백과같은 문자가 포함될 경우 제대로 인식되지 않는 문제해결
 app.use(bodyParser.urlencoded({ extended: true}))
-
+app.use(morgan("dev"))
 
 //미들웨어
 app.use("/", home)
